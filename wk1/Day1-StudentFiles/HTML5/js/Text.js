767a38bfc77c809ac9df7a24cc77cc83e0c2b1f5
Samantha <miller.m.samantha@gmail.com>
2014-02-04 16:50:10 -0500

    Text in canvas

--- wk1/Day1-StudentFiles/HTML5/js/Text.js
+++ wk1/Day1-StudentFiles/HTML5/js/Text.js
@@ -1,13 +1,22 @@
-window.onload = function() {
+window.onload = function(){
 	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
+	if(theCanvas && theCanvas.getContext){
 	var ctx = theCanvas.getContext("2d");
-	if (ctx) {
-						
-					console.log('it works');	
-						
-						
-						
+	if (ctx){
+
+		// .fillText(textString, x, y, maxWidth)
+		var theString = "Fish Lips";
+		// Draw the string using default settings
+		ctx.fillText(theString, 20, 20);
+		ctx.font = "25pt Georgia";
+		ctx.fillText(theString, 20, 60);
+		ctx.fillStyle = 'orange';
+		ctx.fillText(theString, 20, 100);
+		ctx.font = "32pt Verdana";
+		ctx.fillStyle = 'yellow';
+		ctx.strokeStyle = 'rgba(0,255,0, 1)';
+		ctx.fillText(theString, 20, 160);
+		ctx.stroke(theString, 20, 160);
 		}
 	}
 }
