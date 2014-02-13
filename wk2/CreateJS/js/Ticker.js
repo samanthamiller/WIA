f2cb34c11bb72129a118cd3cb97156b9cfcdfba8
Samantha <miller.m.samantha@gmail.com>
2014-02-13 16:27:33 -0500

    Class demo

--- wk2/CreateJS/js/Ticker.js
+++ wk2/CreateJS/js/Ticker.js
@@ -8,9 +8,24 @@ window.onload = function() {
 	rect1.y = 100;
 
 	stage.addChild(rect1);
-	stage.update();		
+		
 
 	// The ticker is a centeralized 'tick' || 'heartbeat' that is broadcast out at a specific interval.
 	// Listeners can hear the tick and create an event that will happen for each tick
+	// Set the interval by setting the frame rate
+	createjs.Ticker.setFPS(100); // FPS(frames/second) defaults to 20
+	// Add and eventListener that will hear the tick
+	createjs.Ticker.addEventListener('tick', tick);
 
+	function tick(){
+		// This function will be called 30 FPS
+		// console.log('I am ticking');
+		rect1.rotation +=8;
+		// rect1.alpha -= .05;
+		// rect1.x ++;
+		// rect1.y ++;
+		// rect1.skewX += 20;
+		// rect1.scaleX += .1;
+		stage.update();	
+	}
 }
